c2774053225bc9d7111b9ff316d0c3554c17e55d
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-09-26 10:32:26 +0900

    :zap: 個別のポケモンのデータを１つ１つ取得

--- src/App.js
+++ src/App.js
@@ -1,49 +1,66 @@
 import './App.css';
+// useEffect をインポート
 import { useEffect, useState } from 'react';
-import PokemonThumbnails from './PokemonThumbnails'
+import PokemonThumbnails from './PokemonThumbnails';
 
 function App() {
 
-  const [pokemonNames, setPokemonNames] = useState([]);
+  const [allPokemons, setAllPokemons] = useState([]);
 
-  // 仮でデータを配列にする
-  const pokemon = [
+  // 仮でデータを作成する
+  // 複数形pokemons に修正
+  const pokemons = [
     {
       id: 1,
-      name: "フシギダネ",
       image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
       type: "くさ"
     },
     {
       id: 2,
-      name: "フシギソウ",
       image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
       type: "くさ"
     },
     {
       id: 3,
-      name: "フシギバナ",
       image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
       type: "くさ"
     },
   ]
 
   // APIからデータを取得する
-  const url = "https://pokeapi.co/api/v2/pokemon";
+  // パラメータにlimitを設定し、20件取得する
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=20");
+  // 仮でフシギダネのURLを使用する
+  const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/bulbasaur"
 
-  useEffect(() => {
+  const getAllPokemons = () => {
     fetch(url)
       .then(res => res.json())
       .then(data => {
-        console.log(data.results[0].name);
-        // 仮で３つのポケモンの名前をセットする
-        const names = [
-          data.results[0].name,
-          data.results[1].name,
-          data.results[2].name,
-        ]
-        setPokemonNames(names);
+        console.log(data.results)
+        setAllPokemons(data.results);
+
+        // 次の20件をURLにセットする
+        setUrl(data.next);
       })
+  }
+
+  const createPokemonObject = () => {
+    fetch(pokemonUrl)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        // ポケモンの画像の場所
+        // - (ハイフン)にlintで自動で半角スペースが入ってしまうため、[]で対応
+        console.log(data.sprites.other["official-artwork"].front_default);
+        // ポケモンのタイプの場所
+        console.log(data.types[0].type.name);
+      })
+  }
+
+  useEffect(() => {
+    getAllPokemons();
+    createPokemonObject();
   }, [])
 
   return (
@@ -51,22 +68,16 @@ function App() {
       <h1>ポケモン図鑑</h1>
       <div className='pokemon-container'>
         <div className='all-container'>
-          {/* 仮で３つの子コンポーネントを表示する */}
-          <PokemonThumbnails
-            id={pokemon[0].id}
-            name={pokemonNames[0]}
-            image={pokemon[0].image}
-            type={pokemon[0].type} />
-          <PokemonThumbnails
-            id={pokemon[1].id}
-            name={pokemonNames[1]}
-            image={pokemon[1].image}
-            type={pokemon[1].type} />
-          <PokemonThumbnails
-            id={pokemon[2].id}
-            name={pokemonNames[2]}
-            image={pokemon[2].image}
-            type={pokemon[2].type} />
+          {pokemons.map((pokemon, index) => (
+            <PokemonThumbnails
+              id={pokemon.id}
+              // 初回レンダリングの際にエラーになるので、オプショナルチェーン(?)をつける
+              name={allPokemons[index]?.name}
+              image={pokemon.image}
+              type={pokemon.type}
+              key={index}
+            />
+          ))}
         </div>
       </div>
     </div>
