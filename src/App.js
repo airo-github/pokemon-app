cba90999a88dd1736c69e9411fbeef94f2f16be7
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-09-26 10:08:14 +0900

    :zap: APIから名前を取得

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import PokemonThumbnails from './PokemonThumbnails'
 
 function App() {
+
+  const [pokemonNames, setPokemonNames] = useState([]);
 
   // 仮でデータを配列にする
   const pokemon = [
@@ -33,7 +35,14 @@ function App() {
     fetch(url)
       .then(res => res.json())
       .then(data => {
-        console.log(data);
+        console.log(data.results[0].name);
+        // 仮で３つのポケモンの名前をセットする
+        const names = [
+          data.results[0].name,
+          data.results[1].name,
+          data.results[2].name,
+        ]
+        setPokemonNames(names);
       })
   }, [])
 
@@ -45,17 +54,17 @@ function App() {
           {/* 仮で３つの子コンポーネントを表示する */}
           <PokemonThumbnails
             id={pokemon[0].id}
-            name={pokemon[0].name}
+            name={pokemonNames[0]}
             image={pokemon[0].image}
             type={pokemon[0].type} />
           <PokemonThumbnails
             id={pokemon[1].id}
-            name={pokemon[1].name}
+            name={pokemonNames[1]}
             image={pokemon[1].image}
             type={pokemon[1].type} />
           <PokemonThumbnails
             id={pokemon[2].id}
-            name={pokemon[2].name}
+            name={pokemonNames[2]}
             image={pokemon[2].image}
             type={pokemon[2].type} />
         </div>
